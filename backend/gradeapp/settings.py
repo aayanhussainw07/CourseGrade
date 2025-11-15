c9eddfb6ef30f130e32181fa5e62566385578eb2
Aayan Hussain <aayanhussainw07@gmail.com>
2025-11-15 18:53:21 -0500

    added cors vercel support

--- backend/gradeapp/settings.py
+++ backend/gradeapp/settings.py
@@ -109,6 +109,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://coursegrade.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
