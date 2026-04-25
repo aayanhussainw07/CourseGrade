cb19c83de2e12f590742473653d097ba13dd88cf
Aayan Hussain <aayanhussainw07@gmail.com>
2026-04-25 11:09:21 -0400

    fixed geist font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
 import { AuthProvider } from "@/components/auth-provider";
 
